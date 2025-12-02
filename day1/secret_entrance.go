7473788fedafe6d6140debf6fd5376e092f7db2f
Derek Lokker <derek.lokker@gmail.com>
2025-12-02 11:16:21 -0800

    part 2

--- day1/secret_entrance.go
+++ day1/secret_entrance.go
@@ -1,3 +1,5 @@
+// Build: go build secret_entrance.go
+// Run: ./secret_entrance --file test3-6
 package main
 
 import (
@@ -8,8 +10,11 @@ import (
 	"strconv"
 )
 
+var (
+	inputFile = flag.String("file", "", "input file to use")
+)
+
 func main() {
-	inputFile := flag.String("file", "", "input file to use")
 	flag.Parse()
 	file, err := os.Open(*inputFile)
 	if err != nil {
@@ -28,21 +33,55 @@ func main() {
 }
 
 func secretEntrance(input []string) {
-	count := 0
+	var c1, c2 int
 	cur := 50
 	for _, i := range input {
 		dir := i[:1]
 		rotate, _ := strconv.Atoi(i[1:])
 		if dir == "R" {
+			c2 += numPasses(cur, rotate)
 			cur += rotate
 		}
 		if dir == "L" {
+			c2 += numPasses(cur, -rotate)
 			cur -= rotate
 		}
 		if cur%100 == 0 {
-			count++
+			c1++
 		}
 		cur = cur % 100
 	}
-	fmt.Printf("Password: %v\n", count)
+	fmt.Printf("Part 1: %v\n", c1)
+	fmt.Printf("Part 2: %v\n", c2)
+}
+
+func numPasses(start, rotate int) int {
+	if start <= 0 && rotate < 0 {
+		if start+rotate <= -100 {
+			return abs((start + rotate) / 100)
+		}
+	}
+	if start < 0 && rotate > 0 {
+		if start+rotate >= 0 {
+			return 1 + abs((start+rotate)/100)
+		}
+	}
+	if start >= 0 && rotate > 0 {
+		if start+rotate >= 100 {
+			return abs((start + rotate) / 100)
+		}
+	}
+	if start > 0 && rotate < 0 {
+		if start+rotate <= 0 {
+			return 1 + abs((start+rotate)/100)
+		}
+	}
+	return 0
+}
+
+func abs(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
 }
